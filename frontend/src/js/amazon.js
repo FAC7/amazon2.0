b0adc72db8dc90af001221fb5f9f4f11ae75d734
tu6619 <tu6619@my.bristol.ac.uk>
2016-03-17 11:44:40 +0000

    navbar update

--- frontend/src/js/amazon.js
+++ frontend/src/js/amazon.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SearchBar from './searchbar/searchbar.js';
+import Nav from './navbar.js';
+
 
 class Amazon extends React.Component{
-    render(){
-        return(
-            <p>Amazon sucks, fAC7 is better!!!</p>
-        )
-    }
+  render(){
+
+    return(
+      <div>
+        <div>
+          <Nav />
+        </div>
+        <div>
+          <SearchBar />
+        </div>
+      </div>
+    )
+  }
 }
 
 ReactDom.render(
-    <Amazon />,
-    document.getElementById('amazon')
+  <Amazon />,
+  document.getElementById('amazon')
 );
--- frontend/src/js/navbar.js
+++ frontend/src/js/navbar.js
@@ -1,62 +1,53 @@
-import React from 'react';
+import React from 'react'
+require('../css/main.css')
 
 class Nav extends React.Component {
 
-    render () {
-        const ulInlineStyle = {
-            backgroundColor: 'green',
-            display: 'inline-flex',
-            justifyContent: 'flex-end',
-            position: 'fixed',
-            height: '15vh',
-            width: '100%',
-            listStyleType: 'none',
-            margin: 'auto',
-            paddingTop: '10px'
+  render () {
+    const ulInlineStyle = {
+      backgroundColor: '#222E3E',
+      display: 'flex',
+      justifyContent: 'flex-end',
+      position: 'fixed',
+      height: '10vh',
+      width: '100%',
+      listStyleType: 'none',
+      margin: 'auto',
+      padding: '10px 0px 10px 0px'
+    };
 
-        };
+    const aInlineStyle = {
+      padding: '10px',
+      color: '#FFFFFF'
+    };
 
-        const aInlineStyle = {
-            padding: '10px',
-            fontFamily: 'verdana'
 
-        };
-
-        const bodyStyle = {
-            margin: '0'
-        };
-
-        return (
-            <div style={bodyStyle}>
-                <ul style={ulInlineStyle}>
-                    <li>
-                        <a style= {aInlineStyle} href={this.props.home}>Home</a>
-                    </li>
-                    <li>
-                        <a style= {aInlineStyle} href={this.props.browse}>Browse</a>
-                    </li>
-                    <li>
-                        <a style= {aInlineStyle} href={this.props.checkout}>Checkout</a>
-                    </li>
-                    <li>
-                        <a style= {aInlineStyle} href={this.props.basket}>Basket</a>
-                    </li>
-                </ul>
-                <div style={{
-                    backgroundColor: 'blue',
-                    height: '100em'
-                }}>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <ul style={ulInlineStyle}>
+          <li>
+            <a style= {aInlineStyle} href={this.props.home}>Home</a>
+          </li>
+          <li>
+            <a style= {aInlineStyle} href={this.props.browse}>Browse</a>
+          </li>
+          <li>
+            <a style= {aInlineStyle} href={this.props.checkout}>Checkout</a>
+          </li>
+          <li>
+            <a style= {aInlineStyle} href={this.props.basket}>Basket</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 Nav.propTypes = {
-    home: React.PropTypes.string,
-    browse: React.PropTypes.string,
-    checkout: React.PropTypes.string,
-    basket: React.PropTypes.string
+  home: React.PropTypes.string,
+  browse: React.PropTypes.string,
+  checkout: React.PropTypes.string,
+  basket: React.PropTypes.string
 };
 
 export default Nav;
