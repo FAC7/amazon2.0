4083d722af74cb23f22f18488ec3d1e7e19f4501
Owen TM <oturnermajor@gmail.com>
2016-03-17 11:20:02 +0000

    reformatted files with standardFormat, changed React files from js to jsx.

---
+++ frontend/src/js/amazon.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+class Amazon extends React.Component {
+  render () {
+    return (
+    <p>
+      Amazon sucks, fAC7 is better!!!
+    </p>
+    )
+  }
+}
+
+ReactDom.render(
+  <Amazon />,
+  document.getElementById('amazon')
+)
