fa3567d0d79d98f61f0ec11ff004709b975b7dcc
franzmoro <franzmoro88@gmail.com>
2016-03-17 17:28:50 +0000

    added callbacks to redisfunctions

--- backend/lib/dbHelpers.js
+++ backend/lib/dbHelpers.js
@@ -2,20 +2,20 @@ const Guid = require('guid');
 
 module.exports = function(client){
   return {
-    addProduct: function( productObj ){
+    addProduct: function( productObj, cb ){
       const productId = Guid.create();
       productObj.id   = productId;
       Object.keys( productObj ).forEach( key => {
         const value = typeof productObj[key] === "object" ?
           JSON.stringify( productObj[key] ) : productObj[key]
-        client.hset( id, key, value, function(err){
+        client.hset( productId, key, value, function(err){
           if(err) {
             console.log('--> was not able to set property ', key, 'to value ', value);
             throw err;
           console.log('successfully set property ', key, 'to value ', value);
           }
         });
-      }
+      });
       const categoriesArr = JSON.parse( productObj.categories );
       categoriesArr.forEach( category => {
         client.SADD( category, productId, function(err){
@@ -27,13 +27,14 @@ module.exports = function(client){
         })
       })
     },
-    getProductById: function(id){
+    getProductById: function(id, cb){
       client.hgetall( id, function(err, reply){
         if(err){
           console.log(' --> was not able to retrieve info for product with id',id);
           throw err;
         }
         console.log('successfully retrieved info for item with id ', id);
+        cb(null,reply);
       });
     },
     getProductsByCategories: function(categoriesArr){
