1f49b67bdfc4af85774f360639d305b8ba666d69
shouston3 <shouston3@me.com>
2016-03-17 14:50:27 +0000

    footer is at a good place to leave for now, styling still needs work

--- frontend/src/js/footer/footer.js
+++ frontend/src/js/footer/footer.js
@@ -1,38 +1,61 @@
 import React from 'react'
 import TopFooter from './topFooter.js'
 
-class Footer extends React.Component{
-  render(){
-    return(
+class Footer extends React.Component {
+  render () {
+    return (
       <footer>
-        <div>
-          <TopFooter heading='Contact' data={content}/>
-        </div>
-        <div>
+        <div style={styles}>
+          <TopFooter heading='Contact' data={contactContent} width='45%' />
+          <TopFooter heading='About' data={aboutContent} width='45%' />
         </div>
       </footer>
     )
   }
 }
 
-var content = [
+var contactContent = [
   {
+    id: 'a',
     content: 'FOUNDERS & CODERS'
   },
   {
+    id: 'b',
     link: 'http://foundersandcoders.com',
     content: 'foundersandcoders.com'
   },
   {
+    id: 'c',
     content: 'hello@foundersandcoders.com'
   },
   {
+    id: 'd',
     content: '+44 20 3583 2442'
   },
   {
+    id: 'e',
     link: 'https://www.google.co.uk/maps/place/14+Palmers+Rd,+London+E2+0SY/@51.5295351,-0.0444836,17z/data=!3m1!4b1!4m2!3m1!1s0x48761d26bfacb2ab:0xf4a25d562f22b01b',
     content: '14 Palmers Road, London, E2 0SY'
   }
 ]
+
+var aboutContent = [
+  {
+    id: 'f',
+    content: 'GitHub Repo',
+    link: 'https://github.com/FAC7/amazon2.0'
+  },
+  {
+    id: 'g',
+    content: 'Issues',
+    link: 'https://github.com/FAC7/amazon2.0/issues'
+  }
+]
+
+var styles = {
+  width: '70%',
+  margin: 'auto',
+  display: 'table'
+}
 
 export default Footer
--- frontend/src/js/footer/topFooter.js
+++ frontend/src/js/footer/topFooter.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-class TopFooter extends React.Component{
+class TopFooter extends React.Component {
   render () {
+    styles.width = this.props.width
     return (
-      <div>
-        <h6>{this.props.heading}</h6>
-        <ul>
+      <div style={styles} >
+        <h3>{this.props.heading}</h3>
+        <ul style={ulStyles}>
           {this.props.data.map((item) => {
-            return <li><a href={item.link}>{item.content}</a></li>
+            return <li key={item.id} ><a href={item.link}>{item.content}</a></li>
           })}
         </ul>
       </div>
@@ -15,9 +16,20 @@ class TopFooter extends React.Component{
   }
 }
 
+var styles = {
+  verticalAlign: 'top',
+  display: 'table-cell'
+}
+
+var ulStyles = {
+  listStyle: 'none',
+  padding: '0'
+}
+
 TopFooter.propTypes = {
   heading: React.PropTypes.string.isRequired,
-  data: React.PropTypes.arrayOf(React.PropTypes.objectOf(React.PropTypes.string)).isRequired
+  data: React.PropTypes.arrayOf(React.PropTypes.objectOf(React.PropTypes.string)).isRequired,
+  width: React.PropTypes.string
 }
 
 export default TopFooter
