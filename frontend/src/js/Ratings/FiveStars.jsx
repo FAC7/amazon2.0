a7cdcb355ab4e5595b7ce2fef861a811af720a0d
Andrew <a.macmurray@icloud.com>
2016-03-18 16:13:55 +0000

    reviewbox now grabs the values of author, review and star rating #13 #36 #17

--- frontend/src/js/Ratings/FiveStars.jsx
+++ frontend/src/js/Ratings/FiveStars.jsx
@@ -3,15 +3,6 @@ import Star from './Star.jsx'
 
 class FiveStars extends React.Component {
   // returns 5 star components with an active class depending on the rating
-
-  clickStars (i) {
-    console.log(i)
-    // toggle class from hidden to visable on Review box div. routing? are we in the review box or not?
-    console.log(this.props.productId)
-    // grab starKey.
-
-  }
-
   rateStars () {
     //
   }
@@ -19,7 +10,7 @@ class FiveStars extends React.Component {
   mapStars () {
     const average = this.props.averageRating
     return [1, 2, 3, 4, 5].map((i) => {
-      let clickFn = this.clickStars.bind(this, i)
+      let clickFn = this.props.clickStars.bind(this, i)
       let activeClass = average > i
       return <Star clickStar={clickFn} key={'star-' + i} active={activeClass} />
     })
--- frontend/src/js/ReviewBox/ReviewBox.jsx
+++ frontend/src/js/ReviewBox/ReviewBox.jsx
@@ -1,14 +1,54 @@
 import React from 'react'
-import Review from './Review.jsx'
 import FiveStars from '../Ratings/FiveStars.jsx'
 
 class ReviewBox extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      review: '',
+      author: '',
+      star: ''
+    }
+  }
+  handleAuthor (e) {
+    e.preventDefault()
+    this.setState({
+      author: e.target.value
+    })
+  }
+  handleReview (e) {
+    e.preventDefault()
+    this.setState({
+      review: e.target.value
+    })
+  }
+  clickStars (i) {
+    this.setState({
+      star: i
+    })
+  }
+
+  handleSubmit (e) {
+    e.preventDefault()
+    console.log(this.state.author, this.state.review, 'stars!!!', this.state.star)
+  }
+
   render () {
     return (
-    <div>
-      <FiveStars {...this.props} />
-      <Review {...this.props} />
-    </div>
+    <form>
+      <FiveStars {...this.props} clickStars={this.clickStars.bind(this)} />
+      <input
+        onChange={this.handleAuthor.bind(this)}
+        type='text'
+        name='author'
+        placeholder='your name'
+        required></input>
+      <br/>
+      <textarea onChange={this.handleReview.bind(this)} rows='4' columns='50'></textarea>
+      <button onClick={this.handleSubmit.bind(this)} type='button'>
+        Submit Review
+      </button>
+    </form>
     )
   }
 }
