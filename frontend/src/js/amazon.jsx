195eac100f6e80db73861bbf95cd883b9ff762f4
JackTierney <jctierney251@gmail.com>
2016-03-18 17:59:17 +0000

    Update amazon.jsx

--- frontend/src/js/amazon.jsx
+++ frontend/src/js/amazon.jsx
@@ -3,21 +3,25 @@ import { render } from 'react-dom'
 import Home from './../modules/home.jsx'
 import Login from './../modules/login.jsx'
 import ReactDom from 'react-dom'
-// import SearchBar from './searchbar/searchbar.jsx'
 import Category from './itemCategory/Category.jsx'
 import SearchBar from './searchbar/searchbar.jsx'
 import ProductPage from './ProductPage/ProductPage.jsx'
 import { Router, Route } from 'react-router'
 require('../css/main.css')
 
+
+// potential future routes
+// import SearchBar from './searchbar/searchbar.jsx'
 // import Products from './../modules/products.jsx'
 // import ProductsCatagories from './../modules/productscatagories.jsx'
 // import ProductsCatagoriesItem  from './../modules/productscatagoriesitem.jsx'
 // import Basket from './../modules/basket.jsx'
 // import Payment from './../modules/payment.jsx'
-
 // /{params*} --- resource handler
 // /404 --- page for 'not found'
+
+
+
 render((
     <Router>
     <Route path="/" component={Home}/>
@@ -28,22 +32,3 @@ render((
 ), document.getElementById('amazon'))
 
 
-
-// <Route path='/products' component={Products}/>
-// <Route path='/basket' component={Basket}/>
-// <Route path='/payment' component={Payment}/>
-
-// class Amazon extends React.Component {
-//   render () {
-//     return (
-//     <div>
-//       <p>
-//         Amazon sucks, fAC7 is better!!!
-//       </p>
-    //   <Category categoryName='category name' />
-    //   <Category categoryName='bouh' />
-    // <ProductPage />
-//     </div>
-//     )
-//   }
-// }
