5747912b2070c858e76eedb386bb406a95fb2cdc
Elias <elias0789@gmail.com>
2016-03-20 23:38:10 +0000

    tidy server

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -1,36 +1,29 @@
 'use strict'
 
-// node modules
 const Hapi = require('hapi')
-const Inert = require('inert')
 
-// plugins
-const payPlugin = require('./payPlugin.js')
+const plugins = [
+  require('inert'),
+  require('./payPlugin.js')
+]
 
-// server config
 const server = new Hapi.Server()
 const port = 4000
 
 server.connection({
-	port: port
+  port: port
 })
 
-// Hapi plugins
-const plugins = [
-	Inert, payPlugin
-]
-
 server.register(plugins, (err) => {
-	if (err) {
-		throw err
-	}
-	server.route({
+  if (err) throw err
+
+  server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
       return reply('hello world')
     }
-	})
+  })
 })
 
 module.exports = server
