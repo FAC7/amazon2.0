6eaca2816f3b8a01e5b0a36a05f8aca6013966dd
RobStallion <Robertefrancis18@gmail.com>
2016-03-21 20:17:19 +0000

    change slider.js so it only displays hairdryer category

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -58,7 +58,7 @@ server.register(plugins, (err) => {
 	    method: 'GET',
 	    path: '/getItemsForCarousel',
 	    handler: function (request, reply) {
-				dbHelpers.getArrayOfProdObjsByCategories(['garden']).then((x) => {
+				dbHelpers.getArrayOfProdObjsByCategories(['appliances', 'electric', 'global']).then((x) => {
 					return reply(JSON.stringify(x))
 				}).catch((y)=>{console.log(y)})
 	    }
