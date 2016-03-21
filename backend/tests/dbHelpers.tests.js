80e3d6f2488b5ddce71bdf67f6fce8b93899b03c
franzmoro <franzmoro88@gmail.com>
2016-03-21 15:20:14 +0000

    cleanup in tests to comply with standardJS. renamed functions to match dbHelpers functions

--- backend/tests/dbHelpers.tests.js
+++ backend/tests/dbHelpers.tests.js
@@ -1,8 +1,10 @@
 // node modules
 var tape = require('wrapping-tape')
 var redis = require('redis')
-var client, test = {}
+var client = {}
+var test = {}
 var dbHelpers
+var populateDB
 // env variables
 var dbUrl = process.env.REDIS_URL || 'redis://localhost:6379'
 var dbNum = process.env.REDIS_TEST_DB || 16
@@ -24,48 +26,49 @@ test = tape({
 })
 
 var testProductObj = {
-  'price': "345",
+  'price': '345',
   'vendor': 'brand1',
   'imageUrl': 'https://img.com',
   'title': "this product is not very descriptive, so you won't find it",
   'description': 'give us your moneeeey!!',
-  'quantity': "1",
+  'quantity': '1',
   'productDetails': "{ 'size':'250x140cm', 'weight':'150kg', 'other':'stuff' }",
-  'averageRating': "3",
+  'averageRating': '3',
   'reviews': '[{"author":"nickname","text":"this product sucks!","rating":"1","date":1458218917974},{"author":"nickname2","text":"this product is awesome!","rating":"5","date":1458218918000}]',
   'categories': '["sports","tech"]'
 }
 var testProductObj2 = {
-  'price': "200",
+  'price': '200',
   'vendor': 'brand2',
   'imageUrl': 'https://img2.com',
   'title': "expensive stuff, don't buy this.",
   'description': 'ripping you off...',
-  'quantity': "2",
+  'quantity': '2',
   'productDetails': "{ 'size':'250x140cm', 'weight':'150kg', 'other':'stuff' }",
-  'averageRating': "1",
+  'averageRating': '1',
   'reviews': '[{"author":"nickname","text":"this product sucks!","rating":"1","date":1458218917974},{"author":"this product is awesome!","text":"this product sucks!","rating":"5","date":1458218918000}]',
   'categories': '["tech"]'
 }
 
 var testProductObj3 = {
-  'price': "10",
+  'price': '10',
   'vendor': 'brand3',
   'imageUrl': 'https://img3.com',
-  'title': "i don't know what this is",
+  'title': 'a superfast sportscar running like crazy at 400km/h',
   'description': 'meh...',
-  'quantity': "3",
+  'quantity': '3',
   'productDetails': "{ 'size':'250x140cm', 'weight':'150kg', 'other':'stuff' }",
-  'averageRating': "1",
+  'averageRating': '1',
   'reviews': '[{"author":"nickname","text":"this product sucks!","rating":1,"date":1458218917974},{"author":"this product is awesome!","text":"this product sucks!","rating":5,"date":1458218918000}]',
   'categories': '["cars"]'
 }
 
 test('testing db Helper getProductById with manual input', (t) => {
-  t.plan(4)
+  t.plan(5)
   client.hmset('h12345', 'title', 'test product new! 500kg', 'price', 500, 'average-rating', 4, (err, reply) => {
     t.ok(!err, 'no error in manually adding a product to the db')
     dbHelpers.getProductById('h12345', (err, obj) => {
+      t.ok(!err, 'no error in getting product by id')
       t.equal(typeof obj, 'object', 'reply from getProductById is an object')
       t.equal(obj.title, 'test product new! 500kg', 'product has correct title')
       t.equal(obj.price, '500', 'product has correct price')
@@ -74,10 +77,11 @@ test('testing db Helper getProductById with manual input', (t) => {
 })
 test('testing db Helper addProduct', (t) => {
   dbHelpers.addProduct(testProductObj, (err, testProductId) => {
+    if (err) {
+      console.log(err)
+    }
     t.plan(6)
     dbHelpers.getProductById(testProductId, (err, reply) => {
-      // reply = JSON.parse(reply)
-      console.log(reply)
       t.ok(!err, 'no error in retrieving product by guid with getProductById')
       t.equal(typeof reply, 'object', 'parsed reply is an object')
       t.equal(typeof reply.id, 'string', 'productid is a string (and not undefined)')
@@ -89,6 +93,9 @@ test('testing db Helper addProduct', (t) => {
 })
 test('testing dbHelper getProductIdsByCategories', (t) => {
   dbHelpers.addProduct(testProductObj, (err, testProductId) => {
+    if (err) {
+      console.log(err)
+    }
     dbHelpers.addProduct(testProductObj2, (err2, testProductId2) => {
       dbHelpers.addProduct(testProductObj3, (err3, testProductId3) => {
         dbHelpers.getProductIdsByCategories(['sports'], (err4, sportsReply) => {
@@ -121,16 +128,16 @@ test('testing dbHelper getProductIdsByCategories', (t) => {
     })
   })
 })
-var newReviewObj = {"author":"angry buyer","text":"where's my money?","rating":3,"date":1458218917954}
-var newReviewObj2 = {"author":"returning angry buyer","text":"where's my money again?","rating":2,"date":1458218917984}
+const newReviewObj = { 'author': 'angry buyer', 'text': 'where\'s my money?', 'rating': 3, 'date': 1458218917954 }
+const newReviewObj2 = { 'author': 'returning angry buyer', 'text': 'where\'s my money again?', 'rating': 2, 'date': 1458218917984 }
 
 test('testing DBHelper getReviewsByProductId', (t) => {
   t.plan(19)
   dbHelpers.addProduct(testProductObj, (err, testProductId) => {
-    t.ok(!err, 'no error in adding product '+testProductId)
+    t.ok(!err, 'no error in adding product ' + testProductId)
 
-    dbHelpers.getReviewsByProductId(testProductId, (err2,reviews) => {
-      t.ok(!err2,'no error in retrieving reviews for product '+testProductId)
+    dbHelpers.getReviewsByProductId(testProductId, (err2, reviews) => {
+      t.ok(!err2, 'no error in retrieving reviews for product ' + testProductId)
       t.ok(reviews instanceof Array, 'reply is in array format')
       t.equal(reviews.length, 2, 'reviews array has two elements')
       t.ok(reviews[0] instanceof Object && reviews[1] instanceof Object, 'items in reviews array are objects')
@@ -140,12 +147,12 @@ test('testing DBHelper getReviewsByProductId', (t) => {
 
       dbHelpers.addReview(testProductId, newReviewObj, (err3, newReview) => {
         t.ok(!err3, 'no error in adding review to product ' + testProductId)
-        t.ok(newReview instanceof Object)
+        t.ok(newReview instanceof Object, 'new review is an object')
         t.ok(newReview.author && newReview.text && newReview.rating && newReview.date, 'added review contains the correct fields')
 
         dbHelpers.addReview(testProductId, newReviewObj2, (err4, newReview2) => {
           t.ok(!err4, 'no error in adding review to product ' + testProductId)
-          t.ok(newReview2 instanceof Object)
+          t.ok(newReview2 instanceof Object, 'new review is an object')
           t.ok(newReview2.author && newReview2.text && newReview2.rating && newReview2.date, 'added review contains the correct fields')
 
           dbHelpers.getReviewsByProductId(testProductId, (err5, reviews2) => {
@@ -162,19 +169,18 @@ test('testing DBHelper getReviewsByProductId', (t) => {
     })
   })
 })
-test('testing dbHelper getProductObjsArrByCategories', (t) => {
-  t.plan(3)
+test('testing dbHelper getArrayOfProdObjsByCategories', (t) => {
+  t.plan(8)
   dbHelpers.addProduct(testProductObj).then(
   dbHelpers.addProduct(testProductObj2)).then(
-  dbHelpers.addProduct(testProductObj3)).then(
-    dbHelpers.getProductObjsArrByCategories(['tech'], (err, reply) => {
+  dbHelpers.addProduct(testProductObj3)).then(() => {
+    dbHelpers.getArrayOfProdObjsByCategories(['tech'], (err, reply) => {
       t.ok(!err, 'no error in retrieving the array of product objects from the database call by categories')
-      reply.sort( (productA, productB) => {
-        return ( productB.averageRating - productA.averageRating )
+      reply.sort((productA, productB) => {
+        return (productB.averageRating - productA.averageRating)
       })
       t.deepEqual(reply[0], testProductObj, 'array\'s first item is correct')
       t.deepEqual(reply[1], testProductObj2, 'array\'s second item is correct')
     })
-  )
 })
   // dbHelpers.getProductObjsArrByCategories()
