bc626bfc4a99f467d1f185c4e2d58954e583a079
RobStallion <Robertefrancis18@gmail.com>
2016-03-21 17:34:30 +0000

    add route to server to get items from DB and send to front end

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -6,12 +6,15 @@ const Inert = require('inert')
 
 // plugins
 const payPlugin = require('./payPlugin.js')
+const client = require('./redis.js')
+const dbHelpers = require('./dbHelpers.js')(client)
 
 // server config
 const server = new Hapi.Server()
 const port = 4000
 
 server.connection({
+	routes: {cors: true},
   port: port
 })
 
@@ -24,13 +27,21 @@ server.register(plugins, (err) => {
   if (err) {
     throw err
   }
-  server.route({
+  server.route([{
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
       return reply('hello world')
     }
-  })
+  },{
+    method: 'GET',
+    path: '/getItemsForCarousel',
+    handler: function (request, reply) {
+			dbHelpers.getProductById('676109a7-ec33-7291-2149-62e398b263b0').then((x) => {
+				return reply(JSON.stringify(x))
+			})
+    }
+  }])
 })
 
 module.exports = server
