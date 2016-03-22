21a04b8efc2c7a02457fdf2df4d0a5feb6f2bdc1
RobStallion <Robertefrancis18@gmail.com>
2016-03-22 15:42:46 +0000

    refactor code in slider.jsx

--- frontend/src/js/itemCategory/Slider.jsx
+++ frontend/src/js/itemCategory/Slider.jsx
@@ -2,6 +2,27 @@ import React from 'react'
 import Item from './Item.jsx'
 import Slick from 'react-slick'
 
+function mapDBtoState(slicedArr, stateArr) {
+  slicedArr.map((el) => {
+    const title = el['title']
+    const imageLink = el['imageLink']
+    const price = el['price']
+    const id = el['id']
+    stateArr.push({
+      itemName: title,
+      price: price,
+      imageUrl: imageLink,
+      id: id
+    })
+  })
+}
+
+function someOtherMapFunction (item) {
+  return (
+    <div> <Item itemName={item.itemName} price={'£ ' + item.price} imageUrl={item.imageUrl} itemID={item.id}/></div>
+  )
+}
+
 class Slider extends React.Component {
 
   constructor () {
@@ -14,43 +35,12 @@ class Slider extends React.Component {
     xhr.onreadystatechange = () => {
       if (xhr.status === 200 && xhr.readyState === 4) {
         const parsed = JSON.parse(xhr.responseText)
-        const slicedArr = parsed.slice(0, 20)
-        slicedArr[0].map((el) => {
-          const title = el['title']
-          const imageLink = el['imageLink']
-          const price = el['price']
-          const id = el['id']
-          this.state.hairdryersArray.push({
-            itemName: title,
-            price: price,
-            imageUrl: imageLink,
-            id: id
-          })
-        })
-        slicedArr[1].map((el) => {
-          const title = el['title']
-          const imageLink = el['imageLink']
-          const price = el['price']
-          const id = el['id']
-          this.state.footballsArray.push({
-            itemName: title,
-            price: price,
-            imageUrl: imageLink,
-            id: id
-          })
-        })
-        slicedArr[2].map((el) => {
-          const title = el['title']
-          const imageLink = el['imageLink']
-          const price = el['price']
-          const id = el['id']
-          this.state.laptopsArray.push({
-            itemName: title,
-            price: price,
-            imageUrl: imageLink,
-            id: id
-          })
-        })
+        const slicedHairdryerArr = parsed[0].slice(0, 20)
+        const slicedFootballArr = parsed[1].slice(0, 20)
+        const slicedLaptopArr = parsed[2].slice(0, 20)
+        mapDBtoState(slicedHairdryerArr, this.state.hairdryersArray)
+        mapDBtoState(slicedFootballArr, this.state.footballsArray)
+        mapDBtoState(slicedLaptopArr, this.state.laptopsArray)
         this.setState(this.state)
       }
     }
@@ -68,21 +58,9 @@ class Slider extends React.Component {
       slidesToShow: 5,
       slidesToScroll: 2
     }
-    let hairdryersProducts = this.state.hairdryersArray.map((item) => {
-      return (
-        <div> <Item itemName={item.itemName} price={'£ ' + item.price} imageUrl={item.imageUrl} itemID={item.id}/></div>
-      )
-    })
-    let footballsProducts = this.state.footballsArray.map((item) => {
-      return (
-        <div> <Item itemName={item.itemName} price={'£ ' + item.price} imageUrl={item.imageUrl} itemID={item.id}/></div>
-      )
-    })
-    let laptopsProducts = this.state.laptopsArray.map((item) => {
-      return (
-        <div> <Item itemName={item.itemName} price={'£ ' + item.price} imageUrl={item.imageUrl} itemID={item.id}/></div>
-      )
-    })
+    let hairdryersProducts = this.state.hairdryersArray.map(someOtherMapFunction)
+    let footballsProducts = this.state.footballsArray.map(someOtherMapFunction)
+    let laptopsProducts = this.state.laptopsArray.map(someOtherMapFunction)
     return (
       <div>
         <h2>Hairdryers</h2>
