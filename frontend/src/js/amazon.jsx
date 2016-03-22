aa2b9579b8f7b0a79ae228e11f79b26eda063835
tu6619 <tu6619@my.bristol.ac.uk>
2016-03-22 12:23:08 +0000

    Router wrapped in stateful parent component.

--- frontend/src/js/amazon.jsx
+++ frontend/src/js/amazon.jsx
@@ -12,6 +12,58 @@ import Payment from './../modules/payment.jsx'
 import { Router, Route } from 'react-router'
 require('../css/main.css')
 
+class App extends React.Component {
+  constructor () {
+    this.state = {
+      searchResults = []
+    }
+    this.search = this.search.bind(this);
+  }
+
+  search () {
+    this.setState({searchResults: [1]})
+  }
+
+  render () {
+    return (
+      <Router>
+        <Route
+          path='/'
+          activeStyle={{ color: 'red' }}
+          component={Home}
+        />
+        <Route
+          path='/home'
+          activeStyle={{ color: 'red' }}
+          component={Home}
+        />
+        <Route
+          path='/basket'
+          activeStyle={{ color: 'red' }}
+          component={Basket}
+        />
+        <Route
+          path='/search'
+          activeStyle={{ color: 'red' }}
+          component={Search}
+          search = { this.search }
+        />
+        <Route
+          path='/products'
+          activeStyle={{ color: 'red' }}
+          component={Products}
+        />
+        <Route
+          path='/payment'
+          activeStyle={{ color: 'red' }}
+          component={Payment}
+        />
+      </Router>
+    );
+  }
+
+
+}
 // potential future routes
 // import SearchBar from './searchbar/searchbar.jsx'
 // import Products from './../modules/products.jsx'
@@ -22,12 +74,5 @@ require('../css/main.css')
 // /{params*} --- resource handler
 // /404 --- page for 'not found'
 render((
-  <Router>
-    <Route path='/' activeStyle={{ color: 'red' }}component={Home} />
-    <Route path='/home' activeStyle={{ color: 'red' }}component={Home} />
-    <Route path='/basket' activeStyle={{ color: 'red' }}component={Basket} />
-    <Route path='/search' activeStyle={{ color: 'red' }}component={Search} />
-    <Route path='/products' activeStyle={{ color: 'red' }}component={Products} />
-    <Route path='/payment' activeStyle={{ color: 'red' }} component={Payment} />
-  </Router>
+  <App/>
   ), document.getElementById('amazon'))
