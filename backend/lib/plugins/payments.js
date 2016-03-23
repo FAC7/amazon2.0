017f74c18311f4b16cd9b502941eb0a4a86328a6
Elias <elias0789@gmail.com>
2016-03-23 23:32:46 +0000

    Use cookie-string utility function

--- backend/lib/plugins/payments.js
+++ backend/lib/plugins/payments.js
@@ -2,6 +2,7 @@
 
 const stripe = require('stripe')(process.env.STRIPE_API_KEY)
 const guid = require('guid')
+const utils = require('../utils.js')
 
 exports.register = (server, options, next) => {
   server.state('am2_pay_data', {
@@ -32,23 +33,19 @@ exports.register = (server, options, next) => {
           source: data.source, // obtained with Stripe.js
           description: data.description
         }, (err, charge) => {
-          if (err) {
-            console.log(err)
-          }
-          const cookieString = {
+          if (err) console.log('Stripe request error: ', err)
+
+          const params = {
             success: !err,
             amount: (!err) ? (charge.amount / 100) : '',
             card_number: (!err) ? charge.source.last4 : '',
             order_number: (!err) ? guid.raw() : '' // spoof an order number
           }
-          const stringKeyVal = Object.keys(cookieString).map((el) => {
-            return el.toString() + '=' + cookieString[el].toString()
-          }).join('%2C')
 
           reply({
             success: !err,
             error: err
-          }).state('am2_pay_data', stringKeyVal)
+          }).state('am2_pay_data', utils.toCookieString(params, '%2C'))
         })
       }
     }
