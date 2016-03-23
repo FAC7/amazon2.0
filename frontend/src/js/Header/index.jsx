21640ca5b7c7322419d9760f074a74f693a19ebd
franzmoro <franzmoro88@gmail.com>
2016-03-23 19:37:47 +0000

    re-added '/checkout' to nav

--- frontend/src/js/Header/index.jsx
+++ frontend/src/js/Header/index.jsx
@@ -14,7 +14,7 @@ class Header extends React.Component {
             categorySelect={this.props.categorySelect}
             handleChange={this.props.handleChange}
             setResultsState={this.props.setResultsState} />
-          <Nav home={'/home'} browse={'/search'} checkout={'/payment'} basket={'/basket'}/>
+          <Nav home={'/home'} browse={'/search'} checkout={'/checkout'} basket={'/basket'}/>
         </header>
       </div>
     )
