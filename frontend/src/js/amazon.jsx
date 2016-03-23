6995b7772c22a5623b2e64477fac51d2bb63fc35
franzmoro <franzmoro88@gmail.com>
2016-03-23 19:01:21 +0000

    completed mergeconflict fixing

--- frontend/src/js/amazon.jsx
+++ frontend/src/js/amazon.jsx
@@ -5,14 +5,8 @@ import PaymentStatus from './PaymentStatus/index.jsx'
 import SearchResults from './SearchResults/index.jsx'
 import ProductPage from './ProductPage/ProductPage.jsx'
 import Basket from './BasketEntry/index.jsx'
-<<<<<<< .merge_file_MowCQg
 import { browserHistory, Router, Route, IndexRoute } from 'react-router'
-=======
 import Checkout from './Checkout/index.jsx'
-import { Router, Route } from 'react-router'
-import querystring from 'querystring'
-import { browserHistory } from 'react-router'
->>>>>>> .merge_file_Q0bNkX
 
 require('../css/main.css')
 
@@ -35,22 +29,7 @@ const App = React.createClass({
       state: this.state,
       changeState: this.changeState
     })
-
-<<<<<<< .merge_file_MowCQg
     return childWithProps
-=======
-  render () {
-    return (
-      <Router history={browserHistory}>
-        <Route path='/' component={Home} search={this.search} categorySelect={this.categorySelect} handleChange={this.handleChange} />
-        <Route path='/basket' activeStyle={{ color: 'red' }} component={Basket} />
-        <Route path='/checkout' activeStyle={{ color: 'red' }} component={Checkout} />
-        <Route path='/payment-status' activeStyle={{ color: 'red' }} component={PaymentStatus} />
-        <Route path='/item/:itemID' component={ProductPage} search={this.search} categorySelect={this.categorySelect} handleChange={this.handleChange} />
-        <Route path='/search?q=:searchString&categories=:categories' component={SearchResults} />
-      </Router>
-    )
->>>>>>> .merge_file_Q0bNkX
   }
 
 })
@@ -60,7 +39,8 @@ render((
     <Route path='/' component={App}>
       <IndexRoute component={Home} />
       <Route path='basket' activeStyle={{ color: 'red' }} component={Basket} />
-      <Route path='/payment' activeStyle={{ color: 'red' }} component={Payment} />
+      <Route path='/checkout' activeStyle={{ color: 'red' }} component={Checkout} />
+      <Route path='/payment-status' activeStyle={{ color: 'red' }} component={PaymentStatus} />
       <Route path='/item/:itemID' component={ProductPage} />
       <Route path='/searchResults' component={SearchResults} />
     </Route>
